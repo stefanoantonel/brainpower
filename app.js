84ffb937d3087221aadb3cc501ae2eed7ed616db
hp <stefanoantonel@gmail.com>
2016-04-14 15:06:51 -0300

    Add app.js for nodeJS

---
+++ app.js
@@ -0,0 +1,36 @@
+#!/bin/env node
+//  Heroku deploy example
+var request = require('request');
+var favicon = require('serve-favicon');
+var express = require('express');
+var fs      = require('fs');
+var app 	  = express();
+var port 	= 8080;
+var index = fs.readFileSync('app/index.html');
+
+
+// app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/app/favicon.ico'));
+app.use(express.static('app'));
+
+app.get("/", function(request, response){ //root dir
+  response.writeHead(200, {'Content-Type': 'html'});
+  response.end(index);
+});
+
+app.get("*", function(request, response){ //root dir
+  response.writeHead(200, {'Content-Type': 'html'});
+  response.end(index);
+});
+
+// app.get('/', function (req, res) {
+// 	res.render('index', {
+// 		title: 'Evangelio',
+// 		gospel: Gospel.gospel,
+// 		reflexion: Gospel.reflexion
+// 	});
+// });
+
+app.listen(port, function(){
+	console.log('listening on '+port);
+});
